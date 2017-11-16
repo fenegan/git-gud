1396c0572bcd7f3f39114aee02890c922bbbb28a
Killian Vermersch <killian.vermersch@supinternet.fr>
2017-11-16 16:39:36 +0100

    Added example commands

---
+++ commands.sh
@@ -0,0 +1,8 @@
+cd # go to home folder
+cd <folder> # change current folder
+
+ls -l # list files in folder
+
+cat <file> # show file
+
+ssh-keygen -t rsa # generate ssh key
---
+++ git.sh
@@ -0,0 +1,13 @@
+git clone <address>
+
+git status
+
+git add <file> # add a file
+git add .      # add all files
+
+git commit -m "message"
+
+git push -u origin master # first push only
+git push
+
+git pull
